f092e097a273208cb921e17d7eacaa2349a596bc
AFND <88117975+AFND@users.noreply.github.com>
2022-11-21 17:56:33 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".typing", {
+    strings: ["Computer Engineer", "Digital Creator", "Writer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing2", {
+    strings: ["Hallo", "привет", "مرحبا"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
